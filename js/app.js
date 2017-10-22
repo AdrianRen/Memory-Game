1c4a6c3dcd3dadbe5166cd226e703b9a393acff7
Adrian Ren <adrian.ren@outlook.com>
2017-10-22 14:45:44 +1300

    app works with many bugs

--- js/app.js
+++ js/app.js
@@ -3,106 +3,167 @@
  */
 
 const cardList = [
-      'fa fa-diamond',
-      'fa fa-diamond',
-      'fa fa-paper-plane-o',
-      'fa fa-paper-plane-o',
-      'fa fa-anchor',
-      'fa fa-anchor',
-      'fa fa-bolt',
-      'fa fa-bolt',
-      'fa fa-cube',
-      'fa fa-cube',
-      'fa fa-leaf',
-      'fa fa-leaf',
-      'fa fa-bicycle',
-      'fa fa-bicycle',
-      'fa fa-bomb',
-      'fa fa-bomb'
-    ],
+    'fa fa-diamond',
+    'fa fa-diamond',
+    'fa fa-paper-plane-o',
+    'fa fa-paper-plane-o',
+    'fa fa-anchor',
+    'fa fa-anchor',
+    'fa fa-bolt',
+    'fa fa-bolt',
+    'fa fa-cube',
+    'fa fa-cube',
+    'fa fa-leaf',
+    'fa fa-leaf',
+    'fa fa-bicycle',
+    'fa fa-bicycle',
+    'fa fa-bomb',
+    'fa fa-bomb'
+  ],
 
-    shuffle = (array) => {
-      let currentIndex = array.length,
-        temporaryValue, randomIndex;
+  shuffle = (array) => {
+    let currentIndex = array.length,
+      temporaryValue, randomIndex;
 
-      while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-      }
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
 
-      return array;
-    },
-    // Shuffle the list of cards using the provided "shuffle" method
-    shuffledList = shuffle(cardList),
+    return array;
+  },
+  // Shuffle the list of cards using the provided "shuffle" method
+  shuffledList = shuffle(cardList),
 
-    deck = document.querySelector('.deck'),
+  deck = document.querySelector('.deck'),
 
-    restart = document.querySelector('.restart'),
+  restart = document.querySelector('.restart'),
 
-    moves = document.querySelector('.moves'),
+  moves = document.querySelector('.moves'),
 
-    stars = document.querySelector('.stars'),
+  stars = document.querySelector('.stars'),
 
-    stopWatch = document.querySelector('.stopWatch'),
+  stopWatch = document.querySelector('.stopWatch'),
 
-    stopwatchMinute = document.querySelector('.minute'),
+  stopwatchMinute = document.querySelector('.minute'),
 
-    stopwatchSecond = document.querySelector('.second'),
+  stopwatchSecond = document.querySelector('.second'),
 
-    modal = document.getElementById('myModal'),
+  modal = document.getElementById('myModal'),
 
-    btn = document.getElementById("myBtn"),
+  btn = document.getElementById("myBtn"),
 
-    span = document.getElementsByClassName("close")[0]
+  span = document.getElementsByClassName("close")[0]
 
-    // Display the card's symbol
-    showSymbol = (e) => {
-      if (e.target.className === 'deck') {
-        return false;
-      } else if (e.target.className.includes(' open show')) {
-        return false;
-      } else {
-        e.target.className += ' open show';
-      }
-    },
-    // Add the card to a *list* of "open" cards
-    addToOpenedCards = (e) => {
+// Display the card's symbol
+showSymbol = (e) => {
+    if (e.target.className === 'deck') {
+      return false;
+    } else if (e.target.className.includes(' open show')) {
+      return false;
+    } else {
+      e.target.className += ' open show';
+    }
+  },
+  // Add the card to a *list* of "open" cards
+  addToOpenedCards = (e) => {
+    if (e.target.className !== 'deck') {
       let symbolClassName = e.target.childNodes[0].className;
+      symbolID = e.target.childNodes[0].id;
       openedCards.push(symbolClassName);
-    },
+      flippedCards.push(symbolID);
+      console.log(`symbolClassName: ${symbolClassName}`);
+      console.log(`symbolID: ${symbolID}`);
+    }
+  },
 
-    movesCounter = (e) => {
-      if (e.target.className === 'card') {
-        counter += 1;
-        counter <= 1 ? moves.innerText = `${counter} Move` : moves.innerText = `${counter} Moves`;
-      }
-    },
+  movesCounter = (e) => {
+    if (e.target.className === 'card') {
+      counter += 1;
+      counter <= 1 ? moves.innerText = `${counter} Move` : moves.innerText = `${counter} Moves`;
+    }
+  },
 
-    gameRating = () => {
-      switch (counter) {
-         case 6:
-            stars.removeChild(stars.firstElementChild);
-            break;
-         case 15:
-        	  stars.removeChild(stars.firstElementChild);
-        	  break;
-      }
-    },
+  gameRating = () => {
+    switch (counter) {
+      case 6:
+        stars.removeChild(stars.firstElementChild);
+        break;
+      case 15:
+        stars.removeChild(stars.firstElementChild);
+        break;
+    }
+  },
 
-    startStopwatch = () => {
-      seconds++;
-      if (seconds > 59) {
-        seconds = 0;
-        minutes++;
+  startStopwatch = () => {
+    seconds++;
+    if (seconds > 59) {
+      seconds = 0;
+      minutes++;
+    }
+    stopwatchSecond.innerHTML = seconds > 9 ? seconds : "0" + seconds;
+    stopwatchMinute.innerHTML = minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00"
+  },
+  /*
+   * set up the event listener for a card. If a card is clicked:
+   *  DONE - display the card's symbol (put this functionality in another function that you call from this one)
+   *  DONE - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+   *  - if the list already has another card, check to see if the two cards match
+   *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+   *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+   *  DONE - increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+   *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+   */
+  flipCards = (e) => {
+    if (e.target.className !== 'deck' && openedCards.length < 2) {
+      if (openedCards.length == 0 && e.target.className !== 'deck') {
+        addToOpenedCards(e);
+        console.log(`LINE: 121 openedCards.length == 0`);
+        console.log(openedCards);
+      } else if (openedCards.length == 1 && e.target.className !== 'deck') {
+        addToOpenedCards(e);
+        console.log(`openedCards: ${openedCards}`);
+        console.log(`openedCards[0]: ${openedCards[0]}`);
+        console.log(`openedCards[1]: ${openedCards[1]}`);
+        if (openedCards[0] == openedCards[1]) {
+          cardsFlipped += 2;
+          console.log(`cardsFlipped: ${cardsFlipped}`);
+          openedCards = [];
+          flippedCards = [];
+          if (cardsFlipped == cardList.length) {
+            modal.style.display = "block";
+          }
+          console.log(`Equal`);
+          console.log(openedCards);
+        } else {
+          flipBack = () => {
+            let card_1 = document.getElementById(flippedCards[0]),
+            card_2 = document.getElementById(flippedCards[1]);
+
+            card_1.parentElement.classList.remove('open');
+            card_1.parentElement.classList.remove('show');
+            card_2.parentElement.classList.remove('open');
+            card_2.parentElement.classList.remove('show');
+            openedCards = [];
+            flippedCards = [];
+          }
+
+          setTimeout(flipBack, 700);
+
+          console.log(`NOT Equal`);
+          console.log(`openedCards: ${openedCards}`);
+          console.log(`flippedCards: ${flippedCards}`);
+        }
       }
-      stopwatchSecond.innerHTML = seconds > 9 ? seconds : "0" + seconds;
-      stopwatchMinute.innerHTML = minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00"
-    };
+    }
+  };
 
 let openedCards = [],
+
+    flippedCards = [],
 
     counter = 0,
 
@@ -110,25 +171,27 @@ let openedCards = [],
 
     minutes = 00,
 
+    cardsFlipped = 0,
+
     interval;
-    // Modal is inspired by w3schools
-    // https://www.w3schools.com/howto/howto_css_modals.asp
-    // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
+// Modal is inspired by w3schools
+// https://www.w3schools.com/howto/howto_css_modals.asp
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 /*
  * Display the cards on the page
@@ -137,39 +200,27 @@ let openedCards = [],
  *   - add each card's HTML to the page
  */
 
-for (let symbol of shuffledList) {
+for (const [index, symbol] of shuffledList.entries()) {
   const listItem = document.createElement('li');
   listItem.setAttribute('class', 'card');
   const itemIcon = document.createElement('i');
   itemIcon.setAttribute('class', symbol);
+  itemIcon.setAttribute('id', symbol + index);
   deck.appendChild(listItem).appendChild(itemIcon);
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  DONE - display the card's symbol (put this functionality in another function that you call from this one)
- *  DONE - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-// set up the event listener for a card. If a card is clicked
-
 // TODO: check is cards empty, true -> trigger the setInterval
-interval = setInterval(startStopwatch, 1000);
-
+if (counter > 0) {
+  interval = setInterval(startStopwatch, 1000);
+}
 
 deck.addEventListener('click', (e) => {
   // e.stopPropagation();
   movesCounter(e);
   gameRating();
   showSymbol(e);
-  addToOpenedCards(e);
+  flipCards(e);
 });
-
 
 
 restart.addEventListener('click', () => {
