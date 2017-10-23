c3324cee5e62f6ba313dfb5461b533932a210105
Adrian Ren <adrian.ren@outlook.com>
2017-10-23 14:26:27 +1300

    Project Completed
    Memory game has been done, but there are many improvement needs to be done.

--- js/app.js
+++ js/app.js
@@ -40,7 +40,9 @@ const cardList = [
 
   deck = document.querySelector('.deck'),
 
-  restart = document.querySelector('.restart'),
+  modal_restart = document.querySelector('#modal_restart'),
+
+  btn_restart = document.querySelector('#btn_restart'),
 
   moves = document.querySelector('.moves'),
 
@@ -56,10 +58,33 @@ const cardList = [
 
   btn = document.getElementById("myBtn"),
 
-  span = document.getElementsByClassName("close")[0]
+  span = document.getElementsByClassName("close")[0],
 
-// Display the card's symbol
-showSymbol = (e) => {
+  modal_moves = document.getElementById('modal_moves'),
+
+  modal_time = document.getElementById('modal_time'),
+
+  modal_rating = document.getElementById('modal_rating'),
+
+  /*
+   * Display the cards on the page
+   *   - shuffle the list of cards using the provided "shuffle" method below
+   *   - loop through each card and create its HTML
+   *   - add each card's HTML to the page
+   */
+
+  generateCards = () => {
+    for (const [index, symbol] of shuffledList.entries()) {
+    const listItem = document.createElement('li');
+    listItem.setAttribute('class', 'card');
+    const itemIcon = document.createElement('i');
+    itemIcon.setAttribute('class', symbol);
+    itemIcon.setAttribute('id', symbol + index);
+    deck.appendChild(listItem).appendChild(itemIcon);
+  }},
+
+  // Display the card's symbol
+  showSymbol = (e) => {
     if (e.target.className === 'deck') {
       return false;
     } else if (e.target.className.includes(' open show')) {
@@ -89,9 +114,11 @@ showSymbol = (e) => {
     switch (counter) {
       case 6:
         stars.removeChild(stars.firstElementChild);
+        modal_rating.innerHTML = 'Game Rating:  &starf;&starf;';
         break;
-      case 15:
+      case 10:
         stars.removeChild(stars.firstElementChild);
+        modal_rating.innerHTML = 'Game Rating:  &starf;';
         break;
     }
   },
@@ -116,28 +143,50 @@ showSymbol = (e) => {
    *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
    */
   flipCards = (e) => {
-    if (e.target.className !== 'deck' && openedCards.length < 2) {
+    if (e.target.className !== 'deck' && openedCards.length < 2 && !e.target.className.includes('open show')) {
       showSymbol(e);
       if (openedCards.length == 0 && e.target.className !== 'deck') {
         addToOpenedCards(e);
       } else if (openedCards.length == 1 && e.target.className !== 'deck') {
         addToOpenedCards(e);
         if (openedCards[0] == openedCards[1]) {
+          let card_1 = document.getElementById(flippedCards[0]),
+              card_2 = document.getElementById(flippedCards[1]);
+          card_1.parentElement.classList.add(`animated`);
+          card_1.parentElement.classList.add(`rubberBand`);
+          card_1.parentElement.classList.add(`match`);
+          card_2.parentElement.classList.add(`animated`);
+          card_2.parentElement.classList.add(`rubberBand`);
+          card_2.parentElement.classList.add(`match`);
           cardsFlipped += 2;
           openedCards = [];
           flippedCards = [];
           if (cardsFlipped == cardList.length) {
             modal.style.display = "block";
+            clearInterval(interval);
+            modal_moves.innerHTML = `Moves: ${counter}`;
+            modal_time.innerHTML = `Time: ${minutes} : ${seconds}`;
           }
         } else {
+          let card_3 = document.getElementById(flippedCards[0]),
+              card_4 = document.getElementById(flippedCards[1]);
+          card_3.parentElement.classList.add(`animated`);
+          card_3.parentElement.classList.add(`wobble`);
+          card_3.parentElement.classList.add(`unmatch`);
+          card_4.parentElement.classList.add(`animated`);
+          card_4.parentElement.classList.add(`wobble`);
+          card_4.parentElement.classList.add(`unmatch`);
           flipBack = () => {
-            let card_1 = document.getElementById(flippedCards[0]),
-              card_2 = document.getElementById(flippedCards[1]);
-
-            card_1.parentElement.classList.remove('open');
-            card_1.parentElement.classList.remove('show');
-            card_2.parentElement.classList.remove('open');
-            card_2.parentElement.classList.remove('show');
+            card_3.parentElement.classList.remove(`animated`);
+            card_3.parentElement.classList.remove(`wobble`);
+            card_3.parentElement.classList.remove(`unmatch`);
+            card_4.parentElement.classList.remove(`animated`);
+            card_4.parentElement.classList.remove(`wobble`);
+            card_4.parentElement.classList.remove(`unmatch`);
+            card_3.parentElement.classList.remove('open');
+            card_3.parentElement.classList.remove('show');
+            card_4.parentElement.classList.remove('open');
+            card_4.parentElement.classList.remove('show');
 
             openedCards = [];
             flippedCards = [];
@@ -163,14 +212,18 @@ let openedCards = [],
   interval,
 
   gameStart = false;
+
+
+window.onload = generateCards();
 // Modal is inspired by w3schools
 // https://www.w3schools.com/howto/howto_css_modals.asp
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
+
+// When the user clicks on <span> (x), close the modal
+
+btn.onclick = () => {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
@@ -182,33 +235,17 @@ window.onclick = function(event) {
   }
 }
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-for (const [index, symbol] of shuffledList.entries()) {
-  const listItem = document.createElement('li');
-  listItem.setAttribute('class', 'card');
-  const itemIcon = document.createElement('i');
-  itemIcon.setAttribute('class', symbol);
-  itemIcon.setAttribute('id', symbol + index);
-  deck.appendChild(listItem).appendChild(itemIcon);
-}
 
 deck.addEventListener('click', (e) => {
-  if (!gameStart) {
+  movesCounter(e);
+  if (!gameStart && counter > 0) {
     gameStart = true;
     interval = setInterval(startStopwatch, 1000);
   };
-  movesCounter(e);
   gameRating();
   flipCards(e);
 });
 
+btn_restart.onclick = () => location.reload();
 
-restart.addEventListener('click', () => {
-
-});
+modal_restart.onclick = () => location.reload();
